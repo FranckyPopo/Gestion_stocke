40c1c5e80da7cf2040b400557fd3ad176145eb00
FranckyPopo <afrifranck2003@gmail.com>
2021-12-11 10:52:27 +0000

    Ajout des conditions pour savoir si l'utilisateur veux continuer et autres

--- exo_gestion_de_stocke.py
+++ exo_gestion_de_stocke.py
@@ -17,30 +17,41 @@ while True:
     if choix == 1:
         print("Pour vous raitaller vous devez entrer le nom du produit et la quantité")
         
-        produit = input("Veuillez entrer le nom du produit: ").lower()
-        quantité = int(input("Veuillez entrer la quantité: "))
-        
-        # On ajoute ou nous fesons la mise a jour du produit
-        if listes_produits.get(produit):
-            listes_produits[produit] += quantité
-        else:
-            listes_produits[produit] = quantité
-        print(listes_produits)
+        while True:
+            produit = input("Veuillez entrer le nom du produit: ").lower()
+            quantité = int(input("Veuillez entrer la quantité: "))
+            
+            # On ajoute ou nous fesons la mise a jour du produit
+            if listes_produits.get(produit):
+                listes_produits[produit] += quantité
+            else:
+                listes_produits[produit] = quantité
+            print(listes_produits)
+            
+            continuer = input("Voulez vous continuer a ajouter des produits O/N ? : ").upper()
+            if continuer == "N":
+                break
+            
             
     elif choix == 2:                    
         print("Pour ajouter un client vous devez entrer sont nom et une addresse mail unique")
 
-        nom_client = input("Veuillez entrer le nom du client: ")
-        email_client = input("Veuillez entrer l'addresse mail du client: ")
-        
-        # On vérifie que l'addresse email du client n'est pas utiliser pas un autres client 
-        if email_client not in listes_clients.values():
-            listes_clients[nom_client] = email_client
-            print("Vous venez d'enregistrer un nouveau client")
+        while True:
+            nom_client = input("Veuillez entrer le nom du client: ")
+            email_client = input("Veuillez entrer l'addresse mail du client: ")
             
-            sleep(3)
-        else:
-            print("Imposible de vous enregistrer cas cette addresse mail a déja été utiliser pour un autres client")
+            # On vérifie que l'addresse email du client n'est pas utiliser pas un autres client 
+            if email_client not in listes_clients.values():
+                listes_clients[nom_client] = email_client
+                print("Vous venez d'enregistrer un nouveau client")
+                
+                sleep(3)
+            else:
+                print("Imposible de vous enregistrer cas cette addresse mail a déja été utiliser pour un autres client")
+                
+            continuer = input("Voulez vous continuer a ajouter un client O/N ? : ").upper()
+            if continuer == "N":
+                break
             
     elif choix == 3:
         print("Pour affectuer une livraison vous devez entrer le nom du produit ainsi que la quantité")
@@ -49,13 +60,10 @@ while True:
             email_livraison = input("Veuillez entrer vôtre addresse email pour effectuer la livraison: ")
             
             if email_livraison in listes_clients.values():
-                produit_livraison = input("Veuillez entrer le nom du produit: ")
+                
+                produit_livraison = input("Veuillez entrer le nom du produit a livrer : ").lower()
                 
                 if produit_livraison in listes_produits.keys():
-                    
-                    if bool(produit_livraison) == False:
-                            print("Livraison effectué avec succès")
-                            break
                     
                     # On vérifie que l'utilisateur ne saisise pas autres choses que des chiffres 
                     try:
@@ -67,18 +75,25 @@ while True:
                     else: 
                         
                         # On verifie la quantité de la livraison
-                        if listes_produits[produit_livraison] > quantite_produit_livraison:
+                        if listes_produits[produit_livraison] >= quantite_produit_livraison:
                             listes_produits[produit_livraison] -= quantite_produit_livraison
+                            
+                            continuer = input("Voulez vous effectuer un autres livraison ? O/N : ").upper()
+                            if continuer == "N":
+                                break
+                            
                         else:
                             print("La quantité du produit en stocke est inférieur a celle demandé")
                             break
                         
                 else:
-                    print("Le produit n'est pas en stocke")
+                    print("Le produit n'est pas en stock")
                     break
                                      
             else:
-                print("Cette addresse mail n'est pas associé a un client, veuillez enregistrer Cette addresse mail en tan que client")    
+                print("Cette addresse mail n'est pas associé a un client, veuillez enregistrer cette addresse mail en tan que client")    
                 sleep(3)
                 break
-        
+    
+    elif choix == 4:
+        pass  

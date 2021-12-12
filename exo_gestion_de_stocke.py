40770dc476ef680d2b82f4b396c2b246bd695b3d
FranckyPopo <afrifranck2003@gmail.com>
2021-12-12 11:32:44 +0000

    Merge et ajout autres

--- exo_gestion_de_stocke.py
+++ exo_gestion_de_stocke.py
@@ -78,28 +78,35 @@ while True:
                         print("Vous de devez saisir un nombre entier comme quantité du produit a livrer")
 
                     else: 
-
                         # On verifie la quantité de la livraison
                         if listes_produits[produit_livraison] >= quantite_produit_livraison:
                             listes_produits[produit_livraison] -= quantite_produit_livraison
                             
+                            
+                            index = liste_emails.index(email_livraison)
+
                             # On recupere l'index de notre client
+                            # ce bout de code n'est pas utile, tu peut le supprimer si tu veux
                             if email_livraison in liste_emails:
                                 print("l'index existe")
-                                index = liste_emails.index(email_livraison)
+                                print(index)
                             
                             # On ajoute la livraison a l'historique de livraison
                             historique_client = {"mail": email_livraison, "liste_produits_livre": {produit_livraison: quantite_produit_livraison}}
-                                                            
+                            
+                            # On ajoute un etudiant s'il n'existe pas 
+
                             # On ajout ou mise a jour du produit selectionné 
                             if (bool(historiques) == False) or (email_livraison not in liste_emails):
                                 print("condition 1")
                                 historiques.append(historique_client)
                                 
                             elif produit_livraison not in historiques[index]["liste_produits_livre"].keys():
+                                print("condition 2")
                                 historiques[index]["liste_produits_livre"][produit_livraison] = quantite_produit_livraison
                                 
                             else:
+
                                 pass
                                 #historiques[i]["liste_produits_livre"][produit_livraison] += quantite_produit_livraison
                             

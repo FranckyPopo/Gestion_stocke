99eecd4f21d2708d791268f94038c5b06831a56f
FranckyPopo <afrifranck2003@gmail.com>
2021-12-12 12:43:59 +0000

    Réorganisation de l'algorithme voir l'historique des livraisons, la mise a jout et l'ajout de nouveau produit son fonctionnelle

--- exo_gestion_de_stocke.py
+++ exo_gestion_de_stocke.py
@@ -80,16 +80,10 @@ while True:
                     else: 
                         # On verifie la quantité de la livraison
                         if listes_produits[produit_livraison] >= quantite_produit_livraison:
-                            listes_produits[produit_livraison] -= quantite_produit_livraison
-                            
-                            
-                            index = liste_emails.index(email_livraison)
+                            listes_produits[produit_livraison] -= quantite_produit_livraison      
 
                             # On recupere l'index de notre client
-                            # ce bout de code n'est pas utile, tu peut le supprimer si tu veux
-                            if email_livraison in liste_emails:
-                                print("l'index existe")
-                                print(index)
+                            index = liste_emails.index(email_livraison)
                             
                             # On ajoute la livraison a l'historique de livraison
                             historique_client = {"mail": email_livraison, "liste_produits_livre": {produit_livraison: quantite_produit_livraison}}
@@ -97,19 +91,17 @@ while True:
                             # On ajoute un etudiant s'il n'existe pas 
 
                             # On ajout ou mise a jour du produit selectionné 
-                            if (bool(historiques) == False) or (email_livraison not in liste_emails):
+                            if (bool(historiques) == False):
                                 print("condition 1")
                                 historiques.append(historique_client)
-                                
-                            elif produit_livraison not in historiques[index]["liste_produits_livre"].keys():
-                                print("condition 2")
-                                historiques[index]["liste_produits_livre"][produit_livraison] = quantite_produit_livraison
-                                
-                            else:
-
-                                pass
-                                #historiques[i]["liste_produits_livre"][produit_livraison] += quantite_produit_livraison
                             
+                            # On gére l'ajout et mise a jour d'un nouveau produit
+                            elif produit_livraison in historiques[index]["liste_produits_livre"].keys():
+                                historiques[index]["liste_produits_livre"][produit_livraison] += quantite_produit_livraison
+                            
+                            elif produit_livraison not in historiques[index]["liste_produits_livre"].keys():
+                                historiques[index]["liste_produits_livre"][produit_livraison] = quantite_produit_livraison
+                                                          
                             print(historiques)
                                     
                             continuer = input("Voulez vous effectuer un autres livraison ? : ").upper()

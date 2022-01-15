055d0625947bb8fae237f500772f0a5bef304e70
FranckyPoop <afrifranck2003@gmail.com>
2022-01-15 14:46:00 +0000

    Récuperation des différentes données

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from time import sleep
 import os
 import json
+import data
 
-  
+dossier_actuel = os.getcwd()
+dossier_donnees = os.path.join(dossier_actuel, "donnees")
+
+# Récuperation des différente données
+recuperation_clients =  data.get_data(dossier_donnees, "liste_clients")
+recuration_produits = data.get_data(dossier_donnees, "liste_produits")
+recuration_historiques = data.get_data(dossier_donnees, "liste_historiques")
+

5b5b288bb7e4ee453af232a0a0be8e058300c77a
iMac-13 <imac-13@iMac-de-iMac-13.local>
2022-01-18 15:56:03 +0000

    Conditional

--- afficher.py
+++ afficher.py
@@ -1,21 +1,34 @@
+from asyncore import write
 from cProfile import label
 from cgitb import text
 from email import header
+from re import template
 from tkinter import*
 from turtle import right, width
 import style
 
+#C'est le titre de la page et la couleur de fond
 afficher = Tk()
 afficher.title("Gestion Des Stock")
 afficher.configure(bg="#808080")
+heading = Label(afficher, text= "Afficher Le Stock", bg="skyblue",highlightbackground="black", highlightthickness= 3, font="Roboto 20").pack(pady=30, ipadx=300, ipady=10)
 
-heading = Label(afficher, text= "Afficher Le Stock", bg="skyblue",highlightbackground="black", highlightthickness= 3, font="Roboto 20").pack(pady=50, ipadx=300, ipady=10)
-
+#Création du cadre qui contiendra le nom du produit et la quantité du produit.
 frame = Frame(afficher, highlightbackground="black", highlightthickness= 3)
-frame.pack(pady=50, ipadx=100, ipady=200)
+frame.pack(pady=30, ipadx=250, ipady=350)
 
-liste = Label(frame, text="La liste Des Produits", font="Roboto 20",).grid(row=0, column=0)
+#Condition pour affiche chaque produit avec sa quantité
+nom_produit = StringVar()
+quantite_produit = StringVar()
 
-btn = Button(frame, text="Retour à la page d'accueil", relief=FLAT, bg="skyblue", width=20, height=3)
+template_stocke = """
+    {}:{}
+    ---------------
+    """
+for produit in recuration_produits:
+    produit_label = Text(frame,"Produit: %s\nQuantité: %s\n--------------" % (nom_produit.get(),quantite_produit.get()))
+    
+#bouton pour revenir à la page d'accueil
+btn = Button(frame , text="Retour à la page d'accueil", font= "Roboto 17",relief=FLAT, bg="skyblue", width=20, height=3).pack(pady= 75)
 
 afficher.mainloop()
